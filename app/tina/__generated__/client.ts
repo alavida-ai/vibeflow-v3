85c54d9fa4667b67cb6fa388d500c04155e5f188
Alexander Girardet <alexandergirardet@gmail.com>
2025-10-06 16:05:27 +0100

    removed app

--- app/tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/alexandergirardet/Marketing/alavida/vibeflow-v3/app/tina/__generated__/.cache/1759761753549', url: 'https://content.tinajs.io/1.6/content/629a1e8f-4838-43e3-8056-4275e76ec729/github/main', token: '835f0fff0ec472fd9b552eea6a55dad2498451c3', queries,  });
-export default client;
-  
